2c80357bc424a1c48df69c9059795021f933d052
393099545@qq.com <cmiao@apple.(none)>
2016-03-27 16:34:31 +0800

    modify pipe

--- SNPcallers.py
+++ SNPcallers.py
@@ -18,8 +18,6 @@ optparser.add_option('-T', '--type', dest = 'datatype',
                      help = 'your data is DNA or RNA ?')
 optparser.add_option('-R', '--ref', dest = 'refpath',
                      help = 'where is your reference seq ?')
-optparser.add_option('-U', '--wu', dest = 'wu',default = 'other',
-                     help = 'where is seq ?')
 optparser.add_option('-O', '--output', dest = 'output',
                      help = 'point your outcome file name.')
 options, args = optparser.parse_args()
@@ -30,7 +28,7 @@ print 'files used to call DNA SNP %s'%DNA_step.namelist
 
 RNA_step = FreebayesPipe('.')
 RNA_step.getreadyfilelist()
-print 'files used to call RNA SNP %s'%RNA_step.namelist
+print 'files used to call RNA SNP: %s'%RNA_step.namelist
 
 def get_seqs(ref):
     '''in order to run SNPcallers simultaneously'''
@@ -51,7 +49,7 @@ def combine_vcf(yournamelist, final_name):
     for i in f1:
         nf.write(i)
     f1.close()
-    for j in yournamelist:
+    for j in yournamelist[1:]:
         f = open(j)
         for line in f:
             if line.startswith('#'):
@@ -77,68 +75,7 @@ def split_namelist(contiglist, n):
         newseqlist.append(nl)
     return newseqlist
 
-def runfreebayesfile(fileslist, ref, output):
-    ''' no mnp and no complex!!!!'''
-    f0 = open('bamfile.fb.list', 'w')
-    for i in fileslist:
-        f0.write(i + '\n')
-    f0.close()
-    f1 = open('run_freebayes.sh', 'w')
-    f1.write('freebayes -f %s -u -X -L bamfile.fb.list > %s\n'%(ref, output))
-    f1.close()
-    call('chmod 777 run_freebayes.sh', shell = True)
-    st = datetime.datetime.now()
-    call('./run_freebayes.sh', shell = True)
-    et = datetime.datetime.now()
-    runtime = et-st
-    m_time = runtime/60
-    h_time = m_time/60
-    print 'run freebayes time: %sh-%sm'%(str(h_time), str(m_time))
-
-def runsambcffile(fileslist, ref, outputname):
-    f0 = open('bamfile.sb.list', 'w')
-    for i in fileslist:
-        f0.write(i + '\n')
-    f0.close()
-    f1 = open('run_samtools.sh', 'w')
-    f1.write('samtools mpileup -f %s -P ILLUMINA -uD -b \
-bamfile.sb.list | bcftools view -cNegv - > %s\n'%(ref, outputname))
-    f1.close()
-    call('chmod 777 run_samtools.sh', shell = True)
-    st = datetime.datetime.now()
-    call('./run_samtools.sh', shell = True)
-    et = datetime.datetime.now()
-    runtime = et-st
-    m_time = runtime/60
-    h_time = m_time/60
-    print 'run samtools time: %sh-%sm'%(str(h_time), str(m_time))
-
-def runGATKfile(fileslist,ref, outputname):
-    '''I dont now wether and how GATK can call a population's SNP.
-    this version is just for one sample'''
-    filenames = []
-    for i in fileslist:
-        filenames.append(i)
-    addI = []
-    for i in filenames:
-        addI.append(' -I ' + i)
-    filearg = ''.join(addI)
-    f1 = open('run_gatk.sh', 'w')
-    f1.write('java -jar /share/Public/cmiao/GATK_tools/\
-GenomeAnalysisTK.jar -nct 10 -allowPotentiallyMisencodedQuals \
--T HaplotypeCaller -L %s -R %s %s -o %s\n'%(seq, ref, filearg, outputname))
-    f1.close()
-    call('chmod 777 run_gatk.sh', shell = True)
-    st = datetime.datetime.now()
-    call('./run_gatk.sh', shell = True)
-    et = datetime.datetime.now()
-    runtime = et-st
-    m_time = runtime/60
-    h_time = m_time/60
-    print 'run GATK time: %sh-%sm'%(str(h_time), str(m_time))
-
-
-def runfreebayesfile1(fileslist, ref, outputname):
+def runfreebayesfile(fileslist, ref, outputname):
     ''' no mnp and no complex!!!!'''
     seqlist = get_seqs(ref)
     f0 = open('bamfile.fb.list', 'w')
@@ -146,62 +83,40 @@ def runfreebayesfile1(fileslist, ref, outputname):
         f0.write(i + '\n')
     f0.close()
     splitnamelist = []
-    if len(seqlist) <= 45:
-        f1 = open('run_freebayes.sh', 'w')
-        for seq in seqlist:
-            f1.write('freebayes -r %s -f %s -u -X -L bamfile.fb.list > %s\n'%(seq,\
+    f1 = open('run_freebayes.sh', 'w')
+    for seq in seqlist:
+        f1.write('freebayes -r %s -f %s -C 2 -F 0.01 -L bamfile.fb.list > %s\n'%(seq,\
 ref, seq+'.'+outputname))
-            splitnamelist.append(seq+'.'+outputname)
-        f1.close()
+        splitnamelist.append(seq+'.'+outputname)
+    f1.close()
+    if len(seqlist) <= 15:
         call('parallel < run_freebayes.sh', shell = True)
         combine_vcf(splitnamelist, outputname)
     else:
-        newseqlist_list = split_namelist(seqlist, 45)
-        n = 0
-        for i in newseqlist_list:
-            n += 1
-            f = open(str(n)+'.fb.parallel.sh','w')
-            for seq in i:
-                f.write('freebayes -r %s -f %s -u -X -L bamfile.fb.list > %s\n'%(seq,\
-ref, seq+'.'+outputname))
-                splitnamelist.append(seq+'.'+outputname)
-            f.close()
-            cmd = 'parallel < %s'%(str(n)+'sb.parallel.sh')
-            call(cmd, shell = True)
+        call('parallel -j 40 < run_freebayes.sh', shell = True)
         combine_vcf(splitnamelist, outputname)
 
-def runsambcffile1(fileslist, ref, outputname):
+def runsambcffile(fileslist, ref, outputname):
     seqlist = get_seqs(ref)
     f0 = open('bamfile.sb.list', 'w')
     for i in fileslist:
         f0.write(i + '\n')
     f0.close()
     splitnamelist = []
-    if len(seqlist) <= 45:
-        f1 = open('run_samtools.sh', 'w')
-        for seq in seqlist:
-            f1.write('samtools mpileup -r %s -f %s -P ILLUMINA -uD -b \
+    f1 = open('run_samtools.sh', 'w')
+    for seq in seqlist:
+        f1.write('samtools mpileup -r %s -f %s -ugDV -b \
 bamfile.sb.list | bcftools view -cNegv - > %s\n'%(seq, ref, seq+'.'+outputname))
-            splitnamelist.append(seq+'.'+outputname)
-        f1.close()
+        splitnamelist.append(seq+'.'+outputname)
+    f1.close()
+    if len(seqlist) <= 15:
         call('parallel < run_samtools.sh', shell = True)
         combine_vcf(splitnamelist, outputname)
     else:
-        newseqlist_list = split_namelist(seqlist, 45)
-        n = 0
-        for i in newseqlist_list:
-            n += 1
-            f = open(str(n)+'.sb.parallel.sh','w')
-            for seq in i:
-                f.write('samtools mpileup -r %s -f %s -P ILLUMINA -uD -b \
-bamfile.sb.list | bcftools view -cNegv - > %s\n'%(seq, ref, seq+'.'+outputname))
-                splitnamelist.append(seq+'.'+outputname)
-            f.close()
-            cmd = 'parallel < %s'%(str(n)+'sb.parallel.sh')
-            call(cmd, shell = True)
+        call('parallel -j 15 < run_samtools.sh', shell = True)
         combine_vcf(splitnamelist, outputname)
 
-def runGATKfile1(fileslist,ref, outputname):
+def runGATKfile(fileslist,ref, outputname):
     '''I dont now wether and how GATK can call a population's SNP.
     this version is just for one sample'''
     seqlist = get_seqs(ref)
@@ -230,32 +145,19 @@ if __name__ == '__main__':
     T = options.datatype
     O = options.output
     R = options.refpath
-    U = options.wu
 
-    if C == 'FB' and T == 'DNA' and U == 'other':
+    if C == 'FB' and T == 'DNA':
         runfreebayesfile(DNA_step.namelist,R,O)
-    elif C == 'GATK' and T == 'DNA' and U == 'other':
+    elif C == 'GATK' and T == 'DNA':
         runGATKfile(DNA_step.namelist,R, O)
-    elif C == 'SB' and T == 'DNA' and U == 'other':
+    elif C == 'SB' and T == 'DNA':
         runsambcffile(DNA_step.namelist,R,O)
-    elif C == 'FB' and T == 'RNA' and U == 'other':
+    elif C == 'FB' and T == 'RNA':
         runfreebayesfile(RNA_step.namelist,R,O)
-    elif C == 'GATK' and T == 'RNA' and U == 'other':
+    elif C == 'GATK' and T == 'RNA':
         runGATKfile(RNA_step.namelist,R, O)
-    elif C == 'SB' and T == 'RNA' and U == 'other':
+    elif C == 'SB' and T == 'RNA':
         runsambcffile(RNA_step.namelist,R, O)
-    elif C == 'FB' and T == 'DNA' and U == 'self':
-        runfreebayesfile1(DNA_step.namelist,R,O)
-    elif C == 'GATK' and T == 'DNA' and U == 'self':
-        runGATKfile1(DNA_step.namelist,R, O)
-    elif C == 'SB' and T == 'DNA' and U == 'self':
-        runsambcffile1(DNA_step.namelist,R,O)
-    elif C == 'FB' and T == 'RNA' and U == 'self':
-        runfreebayesfile1(RNA_step.namelist,R,O)
-    elif C == 'GATK' and T == 'RNA' and U == 'self':
-        runGATKfile1(RNA_step.namelist,R, O)
-    elif C == 'SB' and T == 'RNA' and U == 'self':
-        runsambcffile1(RNA_step.namelist,R, O)
     else:
         print 'Please choose the caller.'
 
